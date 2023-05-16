a35c3cce91863c82acc78fcf3fe7c0d9a0f4360b
nico <nico.mv1998@gmail.com>
2023-05-15 21:02:22 -0400

    creando funcion para eliminar elementos de la tabla de gastos y reajustando saldo y gastos correspondientes

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,37 +31,102 @@ const nombreGasto = document.getElementById('input-nombre-gasto')
 const gasto = document.getElementById('input-gasto')
 const btnGasto = document.getElementById('btn-gasto')
 
-const montoPresupuesto = document.getElementById('monto-presupuesto')  
-const montoGasto = document.getElementById('monto-gasto')
+let montoPresupuesto = document.getElementById('monto-presupuesto')  
+let montoGasto = document.getElementById('monto-gasto')
 
-const montoSaldo = document.getElementById('saldo')
+let montoSaldo = document.getElementById('saldo')
+let tabla = []
 
 let saldo = 0
 let sumarPresupuesto = 0
 let sumarGastos = 0
 
 //FUNCIONES
+// function inyectarMonto(valor,lugar,sumar){
+//     sumarPresupuesto = parseInt(valor.value) + sumar
+//     sumarGastos = parseInt(valor.value) + sumar
+//     if(valor.value == ''){
+//         alert('ingrese un valor valido')
+//        }else{
+//            lugar.innerHTML = `$ ${sumar}`
+//        }
+// }
+
 function inyectarPresupuesto(){
-    sumarPresupuesto = parseInt(presupuesto.value) + sumarPresupuesto
     if(presupuesto.value == ''){
      alert('ingrese un valor valido')
     }else{
+        sumarPresupuesto = parseInt(presupuesto.value) + sumarPresupuesto
         montoPresupuesto.innerHTML = `$ ${sumarPresupuesto}`
     }
 }
 
 function inyectarGastos(){
-    sumarGastos = parseInt(gasto.value) + sumarGastos
-    if(gasto.value == ''){
-        alert('ingrese un valor valido')
-       }else{
+    if(gasto.value !== ''){
+        sumarGastos = parseInt(gasto.value) + sumarGastos
         montoGasto.innerHTML = `$ ${sumarGastos}`
-       }
+    }
 }
 
 function inyectarSaldo(){
     saldo = sumarPresupuesto - sumarGastos
     return montoSaldo.innerHTML = `$ ${saldo}`
+    
+}
+
+function construirObjetoGastos(){
+    const objGastoTabla = {
+        nombreGastos: nombreGasto.value,
+        valor: gasto.value,
+        id: Date.now(),
+    }
+    crearArreglo(objGastoTabla)
+}
+
+function crearArreglo(objGastoTabla){
+    tabla = [...tabla, objGastoTabla]
+    console.log(tabla)
+    plastarTabla(tabla)
+}
+
+function plastarTabla(arreglo){
+    let elementTr = document.getElementById('tbody')
+
+    if(nombreGasto.value !== '' && gasto.value !== ''){
+        elementTr.innerHTML = ``
+        arreglo.map(e => {
+            elementTr.innerHTML += `
+                <tr>
+                    <td>${e.nombreGastos}</td>
+                    <td>${e.valor}</td>
+                    <td><a onclick="eliminarGasto(${e.id})" class="btn btn-sm btn-outline-danger"><i class="fas fa-trash-alt"></i></a></td>
+                </tr>
+            `
+        })
+    }else{
+        alert('Todos los campos son obligatorios')
+    }
+}
+
+function eliminarGasto(id){
+    tabla = tabla.filter(gasto => {
+        if(id !== gasto.id){
+            return gasto
+        }else{
+            reajustarValoresGastos(gasto)
+        }
+    })
+        plastarTabla(tabla) 
+}
+
+function reajustarValoresGastos(gasto){
+    saldo = parseInt(saldo) + parseInt(gasto.valor)
+    sumarGastos = sumarGastos - gasto.valor
+    montoSaldo.innerHTML =`$ ${saldo}`
+    montoGasto.innerHTML = `$ ${sumarGastos}`
+
+    console.log(typeof saldo, typeof sumarGastos)
+    
 }
 
 //EVENTOS
@@ -72,8 +137,9 @@ btnPresupuesto.addEventListener('click', () => {
 
 btnGasto.addEventListener('click', () => {
     inyectarGastos()
+    // inyectarMonto()
     inyectarSaldo()
-    
+    construirObjetoGastos()
 } )
 
 
